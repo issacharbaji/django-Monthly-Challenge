f70ebd7b1149a175cc788c988bc8e5eba5870f82
Issa Charbaji <80466126+issacharbaji@users.noreply.github.com>
2022-11-08 14:52:03 +0200

    Add dictionary for each month challenge + add view.html

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This is a challenge view")
+challenges = {
+    'January':'No Thumbs!',
+    'February':'The other hand!',
+    'March':'Pick it up with your feet!',
+    'April':'Build a house of cards!',
+    'May':'Seven second games',
+    'June':'Catch coins!',
+    'July':'Can you wiggle your face!',
+    'August':'Different accents!',
+    'September':'Blindfold',
+    'October':'One minute games',
+    'November':'Not my arms',
+    'December':'Guess who'
+}
+
+def index(request,challenge):
+    
+    try:
+        check = challenges[challenge]
+        return render(request,'challenges/view.html',context=challenges)
+    except:
+        raise Http404("404 GENERIC ERROR")
