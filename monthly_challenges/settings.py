ec82827c910df22dd72b581caf302fcd21eaf74e
Issa Charbaji <80466126+issacharbaji@users.noreply.github.com>
2022-11-10 12:25:42 +0200

    Add static files + some css styling

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x1t%_d%7^fy52#rdot-6gqxi@o99avw5ux8z-qn4lgn)rfoyh)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / "static" ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
